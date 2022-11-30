c18c94a3afba5355e6e0f971194628e2c92c1095
mmanningpk <75436559+mmanningpk@users.noreply.github.com>
2022-11-30 13:52:24 -0500

    Fix calender view with firestore service

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -29,10 +30,10 @@ class _WodHomeState extends State<WodHome> {
   CalendarFormat format = CalendarFormat.month;
   DateTime selectedDay = DateTime.now();
   DateTime focusedDay = DateTime.now();
-  Map<DateTime,List<Wod>> _calendarWods;
+  Map<DateTime,List<QueryDocumentSnapshot<Object>>> _calendarWods;
   DateFormat _dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss.sss');
 
-  List<Wod> _getWodsForDay(DateTime date){
+  List<QueryDocumentSnapshot> _getWodsForDay(DateTime date){
     date = DateTime.parse(_dateFormat.format(date));
     return _calendarWods[date] ?? [];
   }
@@ -48,8 +49,7 @@ class _WodHomeState extends State<WodHome> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<QuerySnapshot>(
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: const Text("WODaily"),
           centerTitle: true,
@@ -68,149 +68,177 @@ class _WodHomeState extends State<WodHome> {
             )
           ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: [
-                  Expanded(
-                    child: ToggleButtons(
-                      constraints: BoxConstraints(
-                        minWidth: (MediaQuery.of(context).size.width -3) / 2,
-                        minHeight: 40.0,
+        body: StreamBuilder(
+          stream: DatabaseService().dbwodsByMonth(_month),
+          builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                  child: CircularProgressIndicator()
+              );
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ToggleButtons(
+                          constraints: BoxConstraints(
+                            minWidth: (MediaQuery
+                                .of(context)
+                                .size
+                                .width - 3) / 2,
+                            minHeight: 40.0,
+                          ),
+                          children: [
+                            Icon(FontAwesomeIcons.listUl),
+                            Icon(FontAwesomeIcons.calendar),
+                          ],
+                          isSelected: _isSelected,
+                          onPressed: (int index) {
+                            setState(() {
+                              for (int i = 0; i < _isSelected.length; i++) {
+                                _isSelected[i] = i == index;
+                                if (index == 1) {
+                                  _calendarMode = true;
+                                } else {
+                                  _calendarMode = false;
+                                }
+                              }
+                            });
+                          },
+                        ),
                       ),
-                      children: [
-                        Icon(FontAwesomeIcons.listUl),
-                        Icon(FontAwesomeIcons.calendar),
-                      ],
-                      isSelected: _isSelected,
-                      onPressed: (int index) {
-                        setState(() {
-                          for (int i = 0; i < _isSelected.length; i++) {
-                            _isSelected[i] = i == index;
-                            if (index == 1) {
-                              _calendarMode = true;
-                            } else {
-                              _calendarMode = false;
-                            }
-                          }
-                        });
-                      },
-                    ),
+                    ],
                   ),
+                  _calendarMode ? getCalenderView(snapshot) : getListView(snapshot),
                 ],
               ),
-              _calendarMode ? getCalenderView() : getListView(),
-            ],
-          ),
-        ),
-        floatingActionButton: _calendarMode ? null : FloatingActionButton(
-            child: const Icon(Icons.add),
-            tooltip: "Add",
-            elevation: 10,
-            onPressed:() async {
-              final Wod newWod = await Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => EnterWodScreen()
-              ));
-              setState(() {
-                // In case they pressed cancel
-                if (newWod != null) {
-                  //In case they entered a workout for a different month
-                  if (DateFormat('MM/dd/yy').parse(newWod.date).month == _month) {
-                    _wodList.insert(0, newWod);
-                  }
+            );
+          },
+      ),
+      floatingActionButton: _calendarMode ? null : FloatingActionButton(
+          child: const Icon(Icons.add),
+          tooltip: "Add",
+          elevation: 10,
+          onPressed:() async {
+            final Wod newWod = await Navigator.push(context, MaterialPageRoute(
+                builder: (context) => EnterWodScreen()
+            ));
+            setState(() {
+              // In case they pressed cancel
+              if (newWod != null) {
+                //In case they entered a workout for a different month
+                if (DateFormat('MM/dd/yy').parse(newWod.date).month == _month) {
+                  _wodList.insert(0, newWod);
                 }
-              });
-            }
-        ),
+              }
+            });
+          }
       ),
     );
   }
 
-  Widget getCalenderView(){
-    updateCalendar();
+  Widget getCalenderView(AsyncSnapshot<QuerySnapshot<Object>> snapshot){
+    updateCalendar(snapshot);
     return Column(
       children: [
           TableCalendar(
-          focusedDay: selectedDay,
-          firstDay: DateTime(2000),
-          lastDay: DateTime(2050),
-          calendarFormat: format,
-          startingDayOfWeek: StartingDayOfWeek.monday,
-          onFormatChanged: (CalendarFormat _format){
-            setState(() {
-              format = _format;
-            });
-          },
+            focusedDay: selectedDay,
+            firstDay: DateTime(2000),
+            lastDay: DateTime(2050),
+            calendarFormat: format,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            onFormatChanged: (CalendarFormat _format){
+              setState(() {
+                format = _format;
+              });
+            },
 
-          eventLoader: (day) {
-            return _getWodsForDay(day);
-          },
+            onPageChanged: (DateTime focusedDay){
+              setState(() {
+                selectedDay = focusedDay;
+                _month = focusedDay.month;
+                print('onPageChanged ' + _month.toString() + ', ' + selectedDay.toString());
+              });
+            },
 
-          // Header format
-          headerStyle: HeaderStyle(
-            titleCentered: true,
-            formatButtonVisible: false
-          ),
+            eventLoader: (day) {
+              return _getWodsForDay(day);
+            },
 
-          // Calendar Style
-          calendarStyle: CalendarStyle(
-            isTodayHighlighted: true,
-            selectedTextStyle: TextStyle(color: Colors.white),
-
-            defaultDecoration: BoxDecoration(
-              color: Colors.transparent,
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(5.0),
+            // Header format
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false
             ),
 
-            markerDecoration: BoxDecoration(
-              color: Colors.black54,
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(5.0),
-            ),
+            // Day selected
+            onDaySelected: (DateTime selectDay, DateTime focusDay){
+              setState(() {
+                print('onDaySelected');
+                selectedDay = selectDay;
+                focusedDay = focusDay;
+              });
+              print(focusedDay);
+            },
+            selectedDayPredicate: (DateTime date){
+              return isSameDay(selectedDay,date);
+            },
 
-            weekendDecoration: BoxDecoration(
-              color: Colors.transparent,
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(5.0),
-            ),
+            // Calendar Style
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              selectedTextStyle: TextStyle(color: Colors.white),
 
-            selectedDecoration: BoxDecoration(
-              color: Colors.blue,
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(5.0),
-            ),
-            todayDecoration: BoxDecoration(
-                color: Colors.blueGrey.shade300,
+              defaultDecoration: BoxDecoration(
+                color: Colors.transparent,
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(5.0),
+              ),
+
+              outsideDecoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+
+              markerDecoration: BoxDecoration(
+                color: Colors.black54,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+
+              weekendDecoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+
+              selectedDecoration: BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+
+              todayDecoration: BoxDecoration(
+                  color: Colors.blueGrey.shade300,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(5.0),
             ),
           ),
-
-          // Day selected
-          onDaySelected: (DateTime selectDay, DateTime focusDay){
-            setState(() {
-              selectedDay = selectDay;
-              focusedDay = focusDay;
-            });
-            print(focusedDay);
-          },
-          selectedDayPredicate: (DateTime date){
-            return isSameDay(selectedDay,date);
-          },
         ),
-        ..._getWodsForDay(selectedDay).map((Wod wod) => Card(
+        ..._getWodsForDay(selectedDay).map((wodSnapshot) => Card(
           elevation: 5,
           margin: EdgeInsets.all(3),
           shape:const OutlineInputBorder(
               borderRadius: BorderRadius.all(Radius.circular(8))
           ),
           child: ListTile(
-              title: Text(DateFormat('MM/dd/yy').format(DateTime.parse(wod.date))),
-              subtitle: Text(wod.type + ":" + wod.description, style: const TextStyle(fontSize: 16.0),),
-              trailing: Text(wod.score),
+              title: Text(DateFormat('MM/dd/yy').format(wodSnapshot['date'].toDate())),
+              subtitle: Text(wodSnapshot['type'] + ":" + wodSnapshot['description'], style: const TextStyle(fontSize: 16.0),),
+              trailing: Text(wodSnapshot['score']),
             ),
         ),
         )
@@ -218,10 +246,9 @@ class _WodHomeState extends State<WodHome> {
     );
   }
 
-  Widget getListView(){
+  Widget getListView(AsyncSnapshot<QuerySnapshot<Object>> snapshot){
     return Expanded(
       child: Column(
-        //mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Row(
             children: <Widget>[
@@ -249,71 +276,60 @@ class _WodHomeState extends State<WodHome> {
                 icon: const Icon(FontAwesomeIcons.angleDoubleRight),
               ),
             ],
-
           ),
           Expanded(
-              child: StreamBuilder(
-                  stream: DatabaseService().dbwodsByMonth(_month),
-                  builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot){
-                    if (!snapshot.hasData){
-                      return Center(
-                        child: CircularProgressIndicator()
-                      );
-                    }
-                    return Container(
-                      child: ListView(
-                        children: snapshot.data.docs.map((wodSnapshot) {
-                          return Card(
-                            elevation: 5,
-                            margin: EdgeInsets.all(3),
-                            shape:const OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(8))
-                            ),
-                            child: ListTile(
-                              // Might not need the year here if we are showing monthly
-                                title: Text(DateFormat('MM/dd/yy').format(wodSnapshot['date'].toDate()),
-                                          style:  const TextStyle(fontSize: 20.0),),
-                                subtitle: Text(wodSnapshot['description'],
-                                            style:  const TextStyle(fontSize: 20.0),),
-                                trailing: Text(wodSnapshot['score'],
-                                            style:  const TextStyle(fontSize: 20.0),),
-                                onTap: () async {
-                                  final Wod editedWod = await Navigator.push(context,MaterialPageRoute(
-                                      builder: (context) => EditWodScreen(workout: wodSnapshot)
-                                  ));
-                                  /*setState(() {
-                                    if (editedWod != null) {
-                                    }
-                                  });*/
-                                },
+            child: Container(
+              child: ListView(
+                children: snapshot.data.docs.map((wodSnapshot) {
+                  return Card(
+                    elevation: 5,
+                    margin: EdgeInsets.all(3),
+                    shape:const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))
+                    ),
+                    child: ListTile(
+                      // Might not need the year here if we are showing monthly
+                        title: Text(DateFormat('MM/dd/yy').format(wodSnapshot['date'].toDate()),
+                                  style:  const TextStyle(fontSize: 20.0),),
+                        subtitle: Text(wodSnapshot['description'],
+                                    style:  const TextStyle(fontSize: 20.0),),
+                        trailing: Text(wodSnapshot['score'],
+                                    style:  const TextStyle(fontSize: 20.0),),
+                        onTap: () async {
+                          final Wod editedWod = await Navigator.push(this.context,MaterialPageRoute(
+                              builder: (context) => EditWodScreen(workout: wodSnapshot)
+                          ));
+                          /*setState(() {
+                            if (editedWod != null) {
+                            }
+                          });*/
+                        },
 
-                                onLongPress: () => showDialog<String>(
-                                    context: context,
-                                    builder: (BuildContext) => AlertDialog(
-                                      title: const Text("Delete Workout?"),
-                                      actions: <Widget>[
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.pop(context, 'Delete');
-                                            print(wodSnapshot.id);
-                                            DatabaseService().deleteWod(wodSnapshot.id);
-                                          },
-                                          child: const Text('Delete'),
-                                        ),
-                                        TextButton(
-                                          onPressed: () => Navigator.pop(context, 'Cancel'),
-                                          child: const Text('Cancel'),
-                                        ),
-                                      ],
-                                    )
-                                )
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    );
-                  }
-              )
+                        onLongPress: () => showDialog<String>(
+                            context: this.context,
+                            builder: (BuildContext) => AlertDialog(
+                              title: const Text("Delete Workout?"),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(this.context, 'Delete');
+                                    print(wodSnapshot.id);
+                                    DatabaseService().deleteWod(wodSnapshot.id);
+                                  },
+                                  child: const Text('Delete'),
+                                ),
+                                TextButton(
+                                  onPressed: () => Navigator.pop(this.context, 'Cancel'),
+                                  child: const Text('Cancel'),
+                                ),
+                              ],
+                            )
+                        )
+                    ),
+                  );
+                }).toList(),
+              ),
+            )
           ),
         ]
       ),
@@ -336,20 +352,21 @@ class _WodHomeState extends State<WodHome> {
     }
   }
 
-  void updateCalendar() {
+  void updateCalendar(AsyncSnapshot<QuerySnapshot<Object>> snapshot) {
     _calendarWods.clear();
-    for(var wod in _wodList){
-      setState(() {
-        DateTime date = DateTime.parse(wod.date);
+    for(var wod in snapshot.data.docs){
+        DateTime date = wod['date'].toDate();
         if (_calendarWods[date]!=null) {
           _calendarWods[date].add(wod);
         } else {
           _calendarWods[date] = [wod];
         }
-      });
     }
   }
 
+  /*
+  Deprecated now that we are using firestore
+   */
   void _delete(int id, int index) async {
     print('Deleting wod with id: $id');
     db.deleteItem(id);
@@ -366,7 +383,7 @@ class _WodHomeState extends State<WodHome> {
       case 'Settings':
         break;
       case 'Timer':
-        Navigator.of(context).push(MaterialPageRoute(
+        Navigator.of(this.context).push(MaterialPageRoute(
             builder: (context) => TimerScreen()));
         break;
       case 'Search':
